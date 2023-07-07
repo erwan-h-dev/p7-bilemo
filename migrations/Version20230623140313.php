f550d6a802bb04c76cc6331607f4c64be2e660cb
erwan Dev <erwan.h.dev@gmail.com>
2023-07-07 10:55:09 +0200

    add fixtures

---
+++ migrations/Version20230623140313.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230623140313 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, adress VARCHAR(255) DEFAULT NULL, postal_code VARCHAR(255) DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, INDEX IDX_1D1C63B319EB6921 (client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B319EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE users DROP FOREIGN KEY FK_1483A5E919EB6921');
+        $this->addSql('DROP TABLE users');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, first_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, last_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, adress VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, postal_code VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, city VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_1483A5E919EB6921 (client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE users ADD CONSTRAINT FK_1483A5E919EB6921 FOREIGN KEY (client_id) REFERENCES client (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B319EB6921');
+        $this->addSql('DROP TABLE utilisateur');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,23 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Entity\Client;
+use App\Entity\Mobile;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
     public function __construct(
-        private UserPasswordHasherInterface $passwordHasher
+        private UserPasswordHasherInterface $passwordHasher,
+        private TagAwareCacheInterface $cache
     ){ }
     
     public function load(ObjectManager $em): void
     {
+        
         $admin = new Client();
+        
+        $client = new Client();
 
         $admin->setUsername('admin')
             ->setRoles(['ROLE_ADMIN'])
+        ;
+
+        $client->setUsername('user')
+            ->setRoles(['ROLE_USER'])
         ;
 
         $hashedPassword = $this->passwordHasher->hashPassword(
@@ -27,8 +38,45 @@ class AppFixtures extends Fixture
         );
         
         $admin->setPassword($hashedPassword);
+        $client->setPassword($hashedPassword);
 
         $em->persist($admin);
+        $em->persist($client);
+
+        for($i = 1; $i <= 10; $i++) {
+
+            $mobile = new Mobile();
+
+            $mobile->setModel("Model $i")
+                ->setMark("Mark $i")
+                ->setShortDescription("Short description $i")
+                ->setLongDescription("Long description $i")
+                ->setCreatedAt(new \DateTimeImmutable())
+                ->setUpdatedAt(new \DateTimeImmutable())
+            ;
+
+            $em->persist($mobile);
+        }
+
+        for ($i = 1; $i <= 10; $i++) {
+
+            $user = new User();
+
+            $user->setFirstName("First name $i")
+                ->setLastName("Last name $i")
+                ->setEmail("email-".$i."@gmail.com")
+                ->setAdress("Adress $i")
+                ->setPostalCode("Postal code $i")
+                ->setCity("City $i")
+            ;
+
+            $client->addUser($user);
+
+            $em->persist($user);
+        }
+        
         $em->flush();
+
+        $this->cache->invalidateTags(["mobilesCache", "usersCache"]);
     }
 }
